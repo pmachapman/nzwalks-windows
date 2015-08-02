d690dedd597ef34050bbb7dcde84a9751b12404d
Peter Chapman <peter@conglomo.co.nz>
2015-08-02 19:17:34 +1200

    Upgraded to Windows 10

---
+++ MainPage.xaml.cs
@@ -0,0 +1,84 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="App.xaml.cs" company="Conglomo">
+// Copyright 2015 Peter Chapman. Please see LICENCE.md for licence details.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Conglomo.NZWalks
+{
+    using System;
+    using Windows.Devices.Geolocation;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Controls.Maps;
+    using Windows.UI.Xaml.Navigation;
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    /// <remarks>
+    /// The Blank Page item template is documented at <c>http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409</c>.
+    /// </remarks>
+    public sealed partial class MainPage : Page
+    {
+        /// <summary>
+        /// Initialises a new instance of the <see cref="MainPage"/> class.
+        /// </summary>
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+            this.Loaded += this.MainPage_Loaded;
+
+            // Set up the credentials
+            this.MainMap.MapServiceToken = "{your_key_here}";
+        }
+
+        /// <summary>
+        /// Handles the Loaded event of the MainPage.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Set up the default position
+            BasicGeoposition position = new BasicGeoposition() { Latitude = -39.2833, Longitude = 175.5667 };
+
+            // Get the GPS position
+            try
+            {
+                Geolocator geolocator = new Geolocator();
+                Geoposition geoposition = await geolocator.GetGeopositionAsync();
+                if (geoposition != default(Geoposition))
+                {
+                    Geocoordinate geocoordinate = geoposition.Coordinate;
+                    if (geocoordinate != default(Geocoordinate))
+                    {
+                        // Make sure the position is in New Zealand
+                        if ((geocoordinate.Point.Position.Longitude > 160 || geocoordinate.Point.Position.Longitude < -172) && geocoordinate.Point.Position.Latitude > -53 && geocoordinate.Point.Position.Latitude < -30)
+                        {
+                            position = new BasicGeoposition() { Latitude = geocoordinate.Point.Position.Latitude, Longitude = geocoordinate.Point.Position.Longitude };
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                // User blocked location, or another error of that sort
+            }
+
+            // Set the map center
+            this.MainMap.Center = new Geopoint(position);
+            this.MainMap.ZoomLevel = 10;
+
+            // Get the tile overlay
+            HttpMapTileDataSource httpSource = new HttpMapTileDataSource("https://nzwalks.azurewebsites.net/geoserver/gwc/service/ve?quadkey={quadkey}&format=image/png&layers=doc_tracks:doc-tracks&srs=EPSG:3857");
+
+            // Add the tile overlay to the map layer
+            MapTileSource tileSource = new MapTileSource(httpSource);
+
+            // Add the map layer to the map
+            this.MainMap.TileSources.Add(tileSource);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,19 +1,16 @@
-# New Zealand Walks for Windows 8.1 and Windows Phone 8.1
+# New Zealand Walks for Windows 10
 View all of the Department of Conservation walking tracks throughout New Zealand on your Android device (requires an Internet connection).
 
 ## Download
 You can download this app from:
- * http://apps.microsoft.com/windows/en-us/app/new-zealand-walks/537963d4-e7da-45d6-a246-309b558569b7
- * http://www.windowsphone.com/en-nz/store/app/new-zealand-walks/6287589a-5807-4c16-bd74-cc559bc24f59
+ * https://www.microsoft.com/en-nz/store/apps/new-zealand-walks/9wzdncrdchv5
 
 ## Requirements
 
 This app requires the following:
- * Windows 8.1
- * Windows Phone 8.1
+ * Windows 10
 
 ## Notes
- * This app was built with Microsoft Visual Studio 2013 Community Edition https://www.visualstudio.com/en-us/news/vs2013-community-vs.aspx
- * To build the app, you will need the Bing Maps SDK for Windows 8.1 Store apps https://visualstudiogallery.msdn.microsoft.com/224eb93a-ebc4-46ba-9be7-90ee777ad9e1
+ * This app was built with Microsoft Visual Studio 2015 Community Edition https://www.visualstudio.com/en-us/news/vs2013-community-vs.aspx
  * Before building and running for the first time, you will need to edit MainPage.xaml.cs, and enter your credentials and map service token. You can get these from https://msdn.microsoft.com/en-us/library/ff428642.aspx
  * This app is primarily a proof-of-concept app for displaying complex layers on mobile devices.
